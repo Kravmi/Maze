d1e59115a2329ba464ac56617b67bc716b8ee6db
Kravmi <144707588+Kravmi@users.noreply.github.com>
2023-10-30 17:04:28 +0300

    Создали два класса для управления игроком и врагом.

--- maze.py
+++ maze.py
@@ -15,6 +15,33 @@ class GameSprite(pg.sprite.Sprite):
         window.blit(self.picture, (self.rect.x, self.rect.y))
 
 
+class Player(GameSprite):
+    def update(self):
+        keys = pg.key.get_pressed()
+        if keys[pg.K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[pg.K_s] and self.rect.y < 420:
+            self.rect.y += self.speed
+        if keys[pg.K_d] and self.rect.x < 620:
+            self.rect.x += self.speed
+        if keys[pg.K_a] and self.rect.x > 5:
+            self.rect.x -= self.speed
+
+
+class Enemy(GameSprite):     
+    direction = 'left'
+    def update(self):
+        if self.rect.x < 470:
+            self.direction = 'right'
+        if self.rect.x > 615:
+            self.direction = 'left'
+        if self.direction == 'left':
+            self.rect.x -= self.speed
+        if self.direction == 'right':
+            self.rect.x += self.speed
+
+
+
 window = pg.display.set_mode((700, 500))
 pg.display.set_caption('Maze')
 background = pg.transform.scale(pg.image.load('background.jpg'), (700, 500))
@@ -24,11 +51,13 @@ pg.mixer.music.play()
 game = True
 FPS = 60
 clock = pg.time.Clock()
-hero = GameSprite('hero.png', 5, 420, 15)
-cyborg = GameSprite('cyborg.png', 420, 280, 10)
+hero = Player('hero.png', 5, 420, 15)
+cyborg = Enemy('cyborg.png', 420, 280, 4)
 treasure = GameSprite('treasure.png', 580, 420, 0)
 while game:
     window.blit(background, (0, 0))
+    hero.update()
+    cyborg.update()
     hero.reset()
     treasure.reset()
     cyborg.reset()
